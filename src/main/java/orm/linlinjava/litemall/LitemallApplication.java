064a6163f87e40ca2ea0b35e4e89327d69916777
sunyinggang <136080416@qq.com>
2020-01-06 22:00:18 +0800

    first commit

---
+++ src/main/java/orm/linlinjava/litemall/LitemallApplication.java
@@ -0,0 +1,13 @@
+package orm.linlinjava.litemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitemallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LitemallApplication.class, args);
+    }
+
+}
